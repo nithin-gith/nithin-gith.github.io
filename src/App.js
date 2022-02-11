feebc00aca1f36701badb3f56cb9805e278d30ec
Nithin Siriguppa <nithin.siriguppa@gmail.com>
2022-02-11 17:07:51 +0530

    intial commit without gui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+    const [place,setplace] = useState('anantapur');
+    var [locData,setlocData] = useState({name:"",temp:""});
+
+  function changeCity(e){
+    var city = e.target.value;
+     setplace(city);
+    }
+  function handleClick(){
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=5763517a0030474991565608221102&q=${place}&days=1&aqi=no&alerts=no`)
+    .then(responce => {
+      if(!responce.ok){
+        alert("no city found")
+      }
+      else{
+
+        responce.json().then(data=>{console.log(data);setlocData({temp:data.current.temp_c,name:data.location.name})})
+      }
+    }
+    )
+  }
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <input placeholder="Name of place" onChange={changeCity} />
+        <button onClick={handleClick} >Search</button>
+      
+      <h1>i live in {locData.name},{locData.temp}</h1>
+
     </div>
   );
+
 }
 
 export default App;
